188e1a8f0a2c6e0370e2c60d459ed3761a9c0d91
amit-semwal <semwalamit510@gmail.com>
2023-05-14 20:34:14 +0530

    check

--- server.js
+++ server.js
@@ -54,10 +54,6 @@ const server = app.listen(
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-  },
 });
 
 io.on("connection", (socket) => {
